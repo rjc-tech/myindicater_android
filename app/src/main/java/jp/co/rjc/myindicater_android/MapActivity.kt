3d13953c1e7217b734c18db5578a7b13e02109be
YuuNi <halhal.halgerda@gmail.com>
2018-03-07 15:25:37 +0900

    画面レイアウト作成中

--- app/src/main/java/jp/co/rjc/myindicater_android/MapActivity.kt
+++ app/src/main/java/jp/co/rjc/myindicater_android/MapActivity.kt
@@ -1,26 +1,23 @@
+package jp.co.rjc.myindicater_android
+
+import android.app.Activity
 import android.os.Bundle
 import android.os.Parcel
 import android.os.Parcelable
 import android.support.v4.app.FragmentActivity
-import com.google.android.gms.maps.CameraUpdateFactory
+import android.support.v7.app.AppCompatActivity
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import jp.co.rjc.myindicater_android.R
 
-class MapsActivity() : FragmentActivity(), OnMapReadyCallback, Parcelable {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private var mMap: GoogleMap? = null
 
-    constructor(parcel: Parcel) : this() {
-
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_maps)
+        setContentView(R.layout.activity_map)
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
@@ -33,23 +30,5 @@ class MapsActivity() : FragmentActivity(), OnMapReadyCallback, Parcelable {
 //        val sydney = LatLng(-34, 151)
 //        mMap!!.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
 //        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<MapsActivity> {
-        override fun createFromParcel(parcel: Parcel): MapsActivity {
-            return MapsActivity(parcel)
-        }
-
-        override fun newArray(size: Int): Array<MapsActivity?> {
-            return arrayOfNulls(size)
-        }
     }
 }
