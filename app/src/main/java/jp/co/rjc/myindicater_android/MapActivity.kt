a099e9476b155f36a0fb01ba37b7562dabf39204
YuuNi <halhal.halgerda@gmail.com>
2018-03-07 17:45:55 +0900

    レイアウトソースにコンパイルエラー有り、実行は可能

--- app/src/main/java/jp/co/rjc/myindicater_android/MapActivity.kt
+++ app/src/main/java/jp/co/rjc/myindicater_android/MapActivity.kt
@@ -1,15 +1,16 @@
 package jp.co.rjc.myindicater_android
 
-import android.app.Activity
 import android.os.Bundle
-import android.os.Parcel
-import android.os.Parcelable
-import android.support.v4.app.FragmentActivity
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import jp.co.rjc.myindicater_android.R
+import org.w3c.dom.Text
+import android.content.Intent
+import android.net.Uri
+
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -21,6 +22,19 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        // 検索ボタンを押したら、入力された文字列から場所を特定してマップの該当の場所へズーム
+        val searchButton = findViewById<Button>(R.id.search_button)
+        searchButton.setOnClickListener {
+            val place = findViewById<TextView>(R.id.search_destination)
+            // googleのAPIでキーワードから場所検索
+            // https://developers.google.com/maps/documentation/android-api/intents?hl=ja
+            val gmmIntentUri = Uri.parse("geo:0,0?q=" + place)
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.`package` = "com.google.android.apps.maps"
+            startActivity(mapIntent)
+        }
+
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
