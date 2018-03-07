1d8388248e6d4b07538d7f958d495a620f020b85
YuuNi <halhal.halgerda@gmail.com>
2018-03-07 10:21:29 +0900

    現時点でのソース(map作成中、エラー有り)

--- app/src/main/java/jp/co/rjc/myindicater_android/MapActivity.kt
+++ app/src/main/java/jp/co/rjc/myindicater_android/MapActivity.kt
@@ -1,14 +1,55 @@
-package jp.co.rjc.myindicater_android
-
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.os.Parcel
+import android.os.Parcelable
+import android.support.v4.app.FragmentActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import jp.co.rjc.myindicater_android.R
 
-class MapActivity : AppCompatActivity() {
+class MapsActivity() : FragmentActivity(), OnMapReadyCallback, Parcelable {
+
+    private var mMap: GoogleMap? = null
+
+    constructor(parcel: Parcel) : this() {
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_map)
-        val hello = findViewById(R.id.text)
+        setContentView(R.layout.activity_maps)
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        val sydney = LatLng(-34, 151)
+        mMap!!.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MapsActivity> {
+        override fun createFromParcel(parcel: Parcel): MapsActivity {
+            return MapsActivity(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MapsActivity?> {
+            return arrayOfNulls(size)
+        }
     }
 }
