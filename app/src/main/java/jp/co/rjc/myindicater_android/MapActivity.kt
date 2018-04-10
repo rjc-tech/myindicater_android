9a29699371213771c096cc6c12b796a1e29f260c
h_miyamoto <h_miyamoto@r-jc.jp>
2018-04-10 18:55:21 +0900

    開発中コミット

--- app/src/main/java/jp/co/rjc/myindicater_android/MapActivity.kt
+++ app/src/main/java/jp/co/rjc/myindicater_android/MapActivity.kt
@@ -1,15 +1,22 @@
 package jp.co.rjc.myindicater_android
 
 import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.os.Parcel
 import android.os.Parcelable
 import android.support.v4.app.FragmentActivity
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import jp.co.rjc.myindicater_android.R
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.places.Places
+
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -21,6 +28,26 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        val searchText = findViewById<EditText>(R.id.editText)
+        val searchButton = findViewById<Button>(R.id.button3)
+        searchButton.setOnClickListener({
+            val word = searchText.text.toString()
+            if(!word.isNullOrEmpty()){
+                val searchTask = SearchDestinationTask()
+                searchTask.execute(word)
+
+                // TODO
+                val mGoogleApiClient = GoogleApiClient.Builder(this)
+                        .addApi(Places.GEO_DATA_API)
+                        .addApi(Places.PLACE_DETECTION_API)
+                        .enableAutoManage(this, GoogleApiClient.OnConnectionFailedListener {
+                        } )
+                        .build()
+                mGoogleApiClient.connect()
+            }
+        })
+
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
--- app/src/main/java/jp/co/rjc/myindicater_android/SearchDestinationTask.kt
+++ app/src/main/java/jp/co/rjc/myindicater_android/SearchDestinationTask.kt
@@ -1,7 +1,38 @@
 package jp.co.rjc.myindicater_android
 
+import android.os.AsyncTask
+import android.support.annotation.NonNull
+import android.util.Log
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URL
+
 /**
  * Created by h_miyamoto on 2018/04/10.
  */
-class SearchDestinationTask {
+class SearchDestinationTask : AsyncTask<String, Void, String>() {
+
+    override fun onPreExecute(){
+    }
+
+    override fun doInBackground(@NonNull vararg word: String?): String {
+        // TODO 確認用
+        val url = URL("https://maps.googleapis.com/maps/api/place/radarsearch/json?location=51.503186,-0.126446&radius=5000&type=museum&key=AIzaSyBisg850iHlVD2SnEEqRC0zQDzWEV4j6L0")
+        val urlConnection = url.openConnection()
+        urlConnection.connect()
+
+        val reader = BufferedReader(InputStreamReader(urlConnection.getInputStream()))
+        var response = ""
+        var line : String? = null
+        while ({ line = reader.readLine(); line }() != null) {
+            response += line
+        }
+
+        return "TEST OK:"+response
+    }
+
+    override fun onPostExecute(response : String){
+        Log.i("PICK", "onPostExecute:"+response)
+
+    }
 }
