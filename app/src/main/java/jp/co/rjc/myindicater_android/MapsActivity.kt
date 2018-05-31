76df295518b8ca4a7dc0c6f462061c37b43c791e
mypc\sin <kabamoto@r-jc.jp>
2018-05-31 18:52:45 +0900

    処理見直し中 ※現在、エラー発生しているため、正常稼働不可の状態

--- app/src/main/java/jp/co/rjc/myindicater_android/MapsActivity.kt
+++ app/src/main/java/jp/co/rjc/myindicater_android/MapsActivity.kt
@@ -1,205 +1,173 @@
 package jp.co.rjc.myindicater_android
 
-import android.support.v7.app.AppCompatActivity
-
-import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import android.os.Bundle
 
 import android.Manifest
-import android.content.Context
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.support.annotation.NonNull
+import android.location.Location
 import android.support.v4.app.ActivityCompat
 import android.util.Log
 import android.widget.Toast
-import android.location.LocationManager
-import android.location.Criteria
 import android.location.LocationListener
-import android.hardware.SensorManager.getAltitude
-import android.location.Location
-import android.location.LocationProvider
-import com.google.android.gms.maps.model.LatLngBounds
+import android.support.v4.app.FragmentActivity
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.maps.*
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
 
+class MapsActivity : FragmentActivity(), OnMapReadyCallback,
+GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+LocationListener, GoogleMap.OnMyLocationButtonClickListener, LocationSource {
 
+    // lateinit：初期化遅らせ　var：可変 val：固定
+    private lateinit var mMap:GoogleMap
+    private lateinit var mGoogleApiClient:GoogleApiClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var onLocationChangedListener: LocationSource.OnLocationChangedListener
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback,LocationListener {
-
-    private lateinit var mMap: GoogleMap
-    private val REQUEST_PERMISSION = 10
-    private lateinit var myLocationManager: LocationManager
+    // ★★PRIORITY_HIGH_ACCURACY           → 出来る限り高精度の位置情報を表示する。
+    // ★★PRIORITY_NO_POWER                → 受動的(自分からはリクエストしない)
+    // ★★PRIORITY_BALANCED_POWER_ACCURACY → ブロックレベルの精度で得たい場合
+    // ★★PRIORITY_LOW_POWER               → 都市レベル (10km)
+    // kotlin用の配列に直すひつようあり
+    private val priority = {LocationRequest.PRIORITY_HIGH_ACCURACY; LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY;
+        LocationRequest.PRIORITY_LOW_POWER; LocationRequest.PRIORITY_NO_POWER}
+    private var locationPriority:Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_maps_main)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
+        setContentView(R.layout.activity_maps)
+
+        // LocationRequest を生成して精度、インターバルを設定
+        // ★★参考URL:https://qiita.com/mattak/items/22be63f57b71287164bf
+        // ★★位置情報取得用   プロパティによって精度や電力消費量などが変わるので、用途によって使い分けが必要
+        locationRequest = LocationRequest.create()
+
+        // 測位の精度、消費電力の優先度
+        locationPriority = priority[1]
+
+        if (locationPriority === priority[0]) {
+            // 位置情報の精度を優先する場合(★★消費電力：大)
+            locationRequest.setPriority(locationPriority)
+            locationRequest.setInterval(5000)
+            locationRequest.setFastestInterval(16)
+        } else if (locationPriority === priority[1]) {
+            // 消費電力を考慮する場合
+            locationRequest.setPriority(locationPriority)
+            // ★★省電力時のインターバル
+            locationRequest.setInterval(60000)
+            // ★★素早く位置情報を取得したいときのインターバル
+            locationRequest.setFastestInterval(16)
+        } else if (locationPriority === priority[2]) {
+            // "city" level accuracy
+            locationRequest.setPriority(locationPriority)
+        } else {
+            // 外部からのトリガーでの測位のみ
+            locationRequest.setPriority(locationPriority)
+        }
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        // ★★Googleが提供しているサービスに対して、接続するためのクライアント
+        mGoogleApiClient = GoogleApiClient.Builder(this).addApi(LocationServices.API).addConnectionCallbacks(this).addOnConnectionFailedListener(this).build()
     }
 
-    // 位置情報許可の確認
-    fun checkPermission():Boolean {
-        // 既に許可している
-        if (ActivityCompat.checkSelfPermission(this,
-                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            Log.v("kyoka", "きょか")
-            // プロバイダーから位置情報の更新
-            val criteria = Criteria()
-            myLocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-            val provider:String = myLocationManager.getBestProvider(criteria, true)
-            val lastLocation = myLocationManager.getLastKnownLocation(provider)
+    // onResumeフェーズに入ったら接続
+    override fun onResume() {
+        super.onResume()
+        mGoogleApiClient.connect()
+    }
 
-            if(lastLocation != null) {
-                setLocation(lastLocation);
-            }
+    // onPauseで切断
+    public override fun onPause() {
+        super.onPause()
+        mGoogleApiClient.disconnect()
+    }
 
-            getlog(lastLocation,"kyoka")
+    override fun onMapReady(googleMap:GoogleMap) {
+        // check permission
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Log.d("debug", "permission granted")
 
+            mMap = googleMap
+            // default の LocationSource から自前のsourceに変更する
+            mMap.setLocationSource(this)
+            // ★★ マップの現在地レイヤーを有効 ⇒ マップ右上に現在地ボタンが表示される
+            // ★★ [現在地ボタン](/maps/documentation/android-api/images/mylocationbutton.png "右上の現在地ボタン
+            // ★★ UiSettings.setMyLocationButtonEnabled(false) を呼び出すと、ボタンをまとめて非表示にできます。
             mMap.setMyLocationEnabled(true)
-            Toast.makeText(this, "Provider=" + provider, Toast.LENGTH_SHORT).show();
-            // 位置情報の更新（checkSelfPermission後じゃなきゃ定義不可）
-            myLocationManager.requestLocationUpdates(provider, 0L, 0f, this)
-
-            return true
-
-//            locationActivity()
-        } else {
-            Log.v("test", "でふぉ")
-            setDefaultLocation()
-            requestLocationPermission()
-            // 仮
-            return false
-        }// 拒否していた場合
-    }
-
-    // 許可を求める
-    private fun requestLocationPermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                        Manifest.permission.ACCESS_FINE_LOCATION)) {
-            ActivityCompat.requestPermissions(this,
-                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_PERMISSION)
-        } else {
-            val toast = Toast.makeText(this,
-                    "許可されないとアプリが実行できません", Toast.LENGTH_SHORT)
-            toast.show()
-
-            ActivityCompat.requestPermissions(this,
-                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                    REQUEST_PERMISSION)
+            mMap.setOnMyLocationButtonClickListener(this)
+        }
+        else{
+            Log.d("debug", "permission error")
+            return
         }
     }
 
-    // 結果の受け取り
-    override fun onRequestPermissionsResult(
-            requestCode: Int,
-            permissions: Array<String>,
-            grantResults: IntArray) {
-        if (requestCode == REQUEST_PERMISSION) {
-            // 使用が許可された
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                //locationActivity()
-            } else {
-                // それでも拒否された時の対応
-                val toast = Toast.makeText(this,
-                        "これ以上なにもできません", Toast.LENGTH_SHORT)
-                toast.show()
-            }
-        }
-    }
-
-    override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {
-        when (status) {
-            LocationProvider.AVAILABLE -> Log.v("Status", "AVAILABLE")
-            LocationProvider.OUT_OF_SERVICE -> Log.v("Status", "OUT_OF_SERVICE")
-            LocationProvider.TEMPORARILY_UNAVAILABLE -> Log.v("Status", "TEMPORARILY_UNAVAILABLE")
-        }
-    }
-
-    /**
-     * 位置情報が更新されたことを検知（位置情報更新時に呼び出し）
-     *
-     * ＃ このコールバックメソッドはLocationListenerをimplementsする必要あり
-     */
+    // ★★ LocationServices.FusedLocationApi.requestLocationUpdatesで位置情報が更新されたら呼ばれる
     override fun onLocationChanged(location: Location) {
-        setLocation(location)
-        getlog(location,"----------")
+        if (onLocationChangedListener != null) {
+            onLocationChangedListener.onLocationChanged(location);
+
+            // 緯度経度取得
+            val lat = location.getLatitude()
+            val lng = location.getLongitude()
+
+            Toast.makeText(this, "location=$lat$lng", Toast.LENGTH_SHORT).show()
+
+            // ★★ 更新された位置情報の緯度経度でマップを作成
+            // Add a marker and move the camera
+            var newLocation = LatLng(lat, lng)
+            mMap.addMarker(MarkerOptions().position(newLocation).title("My Location"))
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(newLocation))
+        }
     }
 
-    override fun onProviderDisabled(provider: String) {
-    }
-    override fun onProviderEnabled(provider: String) {
-    }
+    // ★★ GoogleApiClient の接続に成功した場合に呼ばれる
+    override fun onConnected(bundle: Bundle?) {
+        // check permission
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Log.d("debug", "permission granted")
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        checkPermission()
-            // Add a marker in Sydney and move the camera
-//            val sydney = LatLng(35.709026, 139.731992)
-            // マーカーに表示するメイン情報と補足情報追加し、マップに表示
-//            mMap.addMarker(MarkerOptions().position(sydney).title("テストメイン").snippet("☆彡補足情報☆彡")).showInfoWindow()
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-//            val cUpdate = CameraUpdateFactory.newLatLngZoom(sydney, 15f)
-//            mMap.moveCamera(cUpdate)
+            // FusedLocationApi
+            // ★★ 位置情報更新・取得処理？→ 位置情報が更新されたら「LocationListener.onLocationChanged」が呼ばれる
+            // TODO FusedLocationProviderApiは非推奨なのでFusedLocationProviderClientを使用するよう修正が必要
+            FusedLocationProviderClient.requestLocationUpdates(mGoogleApiClient, locationRequest, this)
+        } else {
+            Log.d("debug", "permission error")
+            return
+        }
     }
 
-
-    private fun setDefaultLocation() {
-        val tokyo = LatLng(35.681298, 139.766247)
-//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(tokyo, 15f))
-        zoomMap(35.681298, 139.766247)
+    // ★★  GoogleApiClient の接続が中断された場合に呼ばれる
+    override fun onConnectionSuspended(i:Int){
+        Log.d("debug", "onConnectionSuspended");
     }
 
-    private fun setLocation(location: Location) {
-        val myLocation = LatLng(location.latitude, location.longitude)
-        mMap.addMarker(MarkerOptions().position(myLocation).title("てすとお"))
-        zoomMap(location.latitude, location.longitude)
-//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 15f))
+    // ★★   GoogleApiClient の接続に失敗した場合に呼ばれる
+    override fun onConnectionFailed(connectionResult: ConnectionResult) {
+        Log.d("debug", "onConnectionFailed");
     }
 
-
-    private fun zoomMap(latitude: Double, longitude: Double) {
-        // 表示する東西南北の緯度経度を設定
-        val south = latitude * (1 - 0.00005)
-        val west = longitude * (1 - 0.00005)
-        val north = latitude * (1 + 0.00005)
-        val east = longitude * (1 + 0.00005)
-
-        // LatLngBounds (LatLng southwest, LatLng northeast)
-        val bounds = LatLngBounds.builder().include(LatLng(south, west)).include(LatLng(north, east)).build()
-
-        val width = resources.displayMetrics.widthPixels
-        val height = resources.displayMetrics.heightPixels
-
-        // static CameraUpdate.newLatLngBounds(LatLngBounds bounds, int width, int height, int padding)
-        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, width, height, 0))
-
+    // ★★ 現在地ボタンがクリックされた場合に呼ばれる
+    override fun onMyLocationButtonClick(): Boolean {
+        Toast.makeText(this, "現在地ボタン押された！！！", Toast.LENGTH_SHORT).show()
+        return false
     }
 
+    // OnLocationChangedListener calls activate() method
+    override fun activate(onLocationChangedListener: LocationSource.OnLocationChangedListener) {
+        this.onLocationChangedListener = onLocationChangedListener
+    }
 
-    private fun getlog(location: Location, str:String) {
-        Log.v("----------", str)
-        Log.v("Latitude", location.latitude.toString())
-        Log.v("Longitude", location.longitude.toString())
-        Log.v("Accuracy", location.accuracy.toString())
-        Log.v("Altitude", location.altitude.toString())
-        Log.v("Time", location.time.toString())
-        Log.v("Speed", location.speed.toString())
-        Log.v("Bearing", location.bearing.toString())
+    override fun deactivate() {
+        this.onLocationChangedListener = null
     }
 }
