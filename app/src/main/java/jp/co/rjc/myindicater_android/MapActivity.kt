cdc9f74cfe3d0114a1585c10bdee3a84fc24fec1
yoshidamariko <m_yoshida@r-jc.jp>
2018-05-31 18:10:35 +0900

    ツールバーのボタンを押すとトーストがでる

--- app/src/main/java/jp/co/rjc/myindicater_android/MapActivity.kt
+++ app/src/main/java/jp/co/rjc/myindicater_android/MapActivity.kt
@@ -1,34 +1,65 @@
 package jp.co.rjc.myindicater_android
 
 import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.os.Parcel
 import android.os.Parcelable
 import android.support.v4.app.FragmentActivity
 import android.support.v7.app.AppCompatActivity
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import jp.co.rjc.myindicater_android.R
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
-class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapActivity : AppCompatActivity(), View.OnClickListener {
 
-    private var mMap: GoogleMap? = null
+    private var toolbar: Toolbar? = null
+    private var button1: Button? = null
+    private var button2: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
-        val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+
+        toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title = "TEST"
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        button1 = findViewById(R.id.goal_button)
+        button1?.setOnClickListener(this)
+        button2 = findViewById(R.id.about_button)
+        button2?.setOnClickListener(this)
+
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+    object ToastView {
+        fun showToast(context: Context, message: String) {
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+        }
+    }
 
-        // Add a marker in Sydney, Australia, and move the camera.
-//        val sydney = LatLng(-34, 151)
-//        mMap!!.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-//        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.goal_button -> {
+                ToastView.showToast(this, "goal_button")
+            }
+            R.id.about_button -> {
+                ToastView.showToast(this, "about")
+            }
+            else -> {
+                ToastView.showToast(this, "else")
+            }
+        }
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            onBackPressed()
+            return true
+        }
+        return false
     }
 }
--- app/src/main/java/jp/co/rjc/myindicater_android/OneFragment.kt
+++ app/src/main/java/jp/co/rjc/myindicater_android/OneFragment.kt
@@ -1,2 +1,77 @@
-package jp.co.rjc.myindicater_android
+/**
+ * Created by Parsania Hardik on 27-Jan-18.
+ */
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.Toolbar
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import jp.co.rjc.myindicater_android.MapActivity
+import jp.co.rjc.myindicater_android.R
+import kotlinx.android.synthetic.*
 
+class OneFragment : Fragment(), View.OnClickListener {
+
+    private var view1: View? = null
+
+    init {
+        var context: Context? = getContext()
+        context = getContext()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+
+    }
+
+    object ToastView {
+        fun showToast(context: Context, message: String) {
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.goal_button -> {
+                MapActivity.ToastView.showToast(context, "goal_button")
+            }
+            else -> {
+                MapActivity.ToastView.showToast(context, "else")
+            }
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        view1 = inflater!!.inflate(R.layout.fragment_one, container, false)
+
+        return view1
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item!!.itemId
+
+        return super.onOptionsItemSelected(item)
+    }
+
+}
